6c8f1069226ae5d1d2452f212aec3441f9152513
nednassim <jn_nedjar@esi.dz>
2021-06-29 20:18:17 +0100

    Table

---
+++ Table/file_pays.py
@@ -0,0 +1,144 @@
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+from ui_pays import Ui_Form as Pays_form
+from ui_ajout_pays import Ui_Form as Ajout_pays_form
+from ui_modifier_pays import Ui_Form as Modifier_pays_form
+
+from classes import *
+
+class AjoutPaysWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ajout_pays_form()
+        self.ui.setupUi(self)
+
+    def closeEvent(self, event):
+        self.ui.code_pays.clear()
+        self.ui.libelle.clear()
+        super(AjoutPaysWindow, self).closeEvent(event)
+
+class ModifierPaysWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Modifier_pays_form()
+        self.ui.setupUi(self)
+
+    def closeEvent(self, event):
+        self.ui.code_pays.clear()
+        self.ui.libelle.clear()
+        super(ModifierPaysWindow, self).closeEvent(event)
+
+class PaysWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Pays_form()
+        self.ui.setupUi(self)
+        self.display_table("SELECT * FROM pays")
+        self.selectedItems = None
+
+        #related_widgets
+        self.widget = AjoutPaysWindow()
+        self.widget2 = ModifierPaysWindow()
+
+        #push_button_connections
+
+        # la recherche sera faite plus tard(
+            # recherche par code_pays
+            # recherche par libelle
+        #)
+
+        #self.ui.rechercher_pays.textChanged.connect(self.rechercher_pays)
+        self.ui.nouveau_pays.clicked.connect(self.widget.show)
+        self.ui.modifier_pays.clicked.connect(self.modifier_pays)
+        self.ui.supprimer_pays.clicked.connect(self.supprimer_pays)
+        self.ui.fermer_pays.clicked.connect(self.close)
+
+        #externel_push_button_connections
+        self.widget.ui.ajouter_ajout_pays.clicked.connect(self.ajouter_ajout_pays)
+        self.widget.ui.fermer_ajout_pays.clicked.connect(self.fermer_ajout_pays)
+        self.widget2.ui.enregistrer_modifier_pays.clicked.connect(self.enregistrer_modifier_pays)
+        self.widget2.ui.fermer_modifier_pays.clicked.connect(self.fermer_modifier_pays)
+
+    def display_table(self, sql):
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall()
+        mydb.commit()
+        row_count = len(myresult)
+        self.ui.pays_table.setRowCount(row_count)
+        for row in range(row_count):
+            self.ui.pays_table.setVerticalHeaderItem(row,QTableWidgetItem(str(myresult[row][0])))
+            self.ui.pays_table.setItem(row, 0, QTableWidgetItem(str(myresult[row][1])))
+            self.ui.pays_table.setItem(row, 1, QTableWidgetItem(myresult[row][2]))
+
+    def rechercher_pays(self):
+        sql = "SELECT * FROM pays WHERE nom LIKE '{}%'".format(self.ui.rechercher_pays.text())
+        self.display_table(sql)
+
+    def modifier_pays(self):
+        self.selectedItems = self.ui.pays_table.selectedItems()
+        if len(self.selectedItems) == 2:
+            self.widget2.show()
+            self.widget2.ui.code_pays.insert(self.selectedItems[0].text())
+            self.widget2.ui.libelle.insertPlainText(self.selectedItems[1].text())
+
+    def supprimer_pays(self):
+        selectedItems = self.ui.pays_table.selectedItems()
+        if len(selectedItems) >= 2:
+            for i in range(0, len(selectedItems), 2):
+                pays_id = int(self.ui.pays_table.verticalHeaderItem(selectedItems[i].row()).text())
+                pays = Pays(pays_id)                
+                pays.supprimer()
+                self.ui.pays_table.removeRow(selectedItems[i].row())
+
+    def ajouter_ajout_pays(self):
+        self.widget.show()
+        code_pays = self.widget.ui.code_pays.text()
+        libelle = self.widget.ui.libelle.toPlainText()
+        pays = Pays(0)
+        pays_id = pays.inserer(code_pays, libelle)
+        
+        # clear line edits after inserting
+        self.widget.ui.code_pays.clear()
+        self.widget.ui.libelle.clear()
+        
+        # displaying the added row
+        availableRow = self.ui.pays_table.rowCount()
+        self.ui.pays_table.insertRow(availableRow)
+        self.ui.pays_table.setVerticalHeaderItem(availableRow, QTableWidgetItem(str(pays_id)))
+        self.ui.pays_table.setItem(availableRow, 0, QTableWidgetItem(code_pays))
+        self.ui.pays_table.setItem(availableRow, 1, QTableWidgetItem(libelle))
+        self.widget.close()
+
+    def fermer_ajout_pays(self):
+        self.widget.ui.code_pays.clear()
+        self.widget.ui.libelle.clear()
+        self.widget.close()
+
+    def enregistrer_modifier_pays(self):
+        current_pays_id = int(self.ui.pays_table.verticalHeaderItem(self.selectedItems[0].row()).text())
+        pays = Pays(current_pays_id)
+        pays.modifier(self.widget2.ui.code_pays.text(), self.widget2.ui.libelle.toPlainText())
+        
+        # update displayed table
+        self.selectedItems[0].setText(self.widget2.ui.code_pays.text())
+        self.selectedItems[1].setText(self.widget2.ui.libelle.toPlainText())
+
+        # clear line edits after modifying
+        self.widget2.ui.code_pays.clear()
+        self.widget2.ui.libelle.clear() 
+        self.widget2.close()
+
+    def fermer_modifier_pays(self):
+        self.widget2.ui.code_pays.clear()
+        self.widget2.ui.libelle.clear()
+        self.widget2.close()
+
+if __name__ == "__main__" :
+    app = QApplication()
+    widget = PaysWindow()
+    widget.show()
+    app.exec()
+
---
+++ Table/ui_modifier_monnaie.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'modifier_monnaiernNYed.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.4
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(455, 235)
+        Form.setStyleSheet(u"background-color:#DCDCDC	")
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(Form)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setStyleSheet(u"background-color: #2f3e40; /*#2f3e46,1a535c;1d3557;233d4d*/\n"
+"color: rgb(255, 255, 255);")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.frame_2)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label = QLabel(self.frame_2)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setFamily(u"Segoe UI")
+        font.setPointSize(14)
+        font.setBold(True)
+        self.label.setFont(font)
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.horizontalSpacer = QSpacerItem(466, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout_2.addWidget(self.frame_2)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setSpacing(12)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.label_3 = QLabel(self.frame_3)
+        self.label_3.setObjectName(u"label_3")
+        palette = QPalette()
+        brush = QBrush(QColor(47, 62, 70, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(220, 220, 220, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Text, brush)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Active, QPalette.PlaceholderText, brush)
+#endif
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush)
+#endif
+        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Text, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush)
+#endif
+        self.label_3.setPalette(palette)
+        self.label_3.setStyleSheet(u"color: #2f3e46;")
+
+        self.verticalLayout_4.addWidget(self.label_3)
+
+        self.label_4 = QLabel(self.frame_3)
+        self.label_4.setObjectName(u"label_4")
+        font1 = QFont()
+        font1.setFamily(u"Segoe UI")
+        font1.setPointSize(9)
+        self.label_4.setFont(font1)
+        self.label_4.setStyleSheet(u"color: #2f3e46;")
+
+        self.verticalLayout_4.addWidget(self.label_4)
+
+        self.label_2 = QLabel(self.frame_3)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setStyleSheet(u"color: #2f3e46;")
+
+        self.verticalLayout_4.addWidget(self.label_2)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_4)
+
+        self.verticalLayout_6 = QVBoxLayout()
+        self.verticalLayout_6.setSpacing(6)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.code_monnaie = QLineEdit(self.frame_3)
+        self.code_monnaie.setObjectName(u"code_monnaie")
+        self.code_monnaie.setStyleSheet(u"border : 1px solid #2f3e46;\n"
+"border-radius:10px;\n"
+"padding: 3px;\n"
+"background-color: white;\n"
+"color: #2f3e46;\n"
+"")
+
+        self.verticalLayout_6.addWidget(self.code_monnaie)
+
+        self.libelle_monnaie = QLineEdit(self.frame_3)
+        self.libelle_monnaie.setObjectName(u"libelle_monnaie")
+        self.libelle_monnaie.setStyleSheet(u"border : 1px solid #2f3e46;\n"
+"border-radius:10px;\n"
+"padding: 3px;\n"
+"background-color: white;\n"
+"color: #2f3e46;\n"
+"")
+
+        self.verticalLayout_6.addWidget(self.libelle_monnaie)
+
+        self.taux_change = QLineEdit(self.frame_3)
+        self.taux_change.setObjectName(u"taux_change")
+        self.taux_change.setStyleSheet(u"border : 1px solid #2f3e46;\n"
+"border-radius:10px;\n"
+"padding: 3px;\n"
+"background-color: white;\n"
+"color: #2f3e46;\n"
+"")
+
+        self.verticalLayout_6.addWidget(self.taux_change)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_6)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_3.addItem(self.verticalSpacer_2)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(-1, 0, -1, 0)
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_2)
+
+        self.enregistrer_modifier_monnaie = QPushButton(self.frame_3)
+        self.enregistrer_modifier_monnaie.setObjectName(u"enregistrer_modifier_monnaie")
+        self.enregistrer_modifier_monnaie.setStyleSheet(u"QPushButton {width: 65px;\n"
+"background-color: white;\n"
+"border: 2px solid #3bb273;\n"
+"color: #3bb273;\n"
+"padding: 6px;\n"
+"border-radius: 10px;}\n"
+"\n"
+"QPushButton:hover {background-color: #3bb273;color: white;}\n"
+"QPushButton:pressed { margin: 1px; }\n"
+"")
+
+        self.horizontalLayout_4.addWidget(self.enregistrer_modifier_monnaie)
+
+        self.fermer_modifier_monnaie = QPushButton(self.frame_3)
+        self.fermer_modifier_monnaie.setObjectName(u"fermer_modifier_monnaie")
+        self.fermer_modifier_monnaie.setStyleSheet(u"QPushButton {background-color: white;\n"
+"border: 2px solid #ee2e31;\n"
+"color: #ee2e31;\n"
+"padding: 5px;\n"
+"border-radius: 10px;}\n"
+"QPushButton:hover {\n"
+"background-color: #ee2e31;\n"
+"color: white;\n"
+"}\n"
+"QPushButton:pressed{\n"
+"margin: 1px;\n"
+"}\n"
+"/*#ee2e31*/")
+
+        self.horizontalLayout_4.addWidget(self.fermer_modifier_monnaie)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+
+
+        self.verticalLayout_2.addWidget(self.frame_3)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Modifier Monnaie", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"Code Monnaie", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"Libell\u00e9 Monnaie", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"Taux de Change", None))
+        self.libelle_monnaie.setText("")
+        self.enregistrer_modifier_monnaie.setText(QCoreApplication.translate("Form", u"Enregistrer", None))
+        self.fermer_modifier_monnaie.setText(QCoreApplication.translate("Form", u"Fermer", None))
+    # retranslateUi
+
